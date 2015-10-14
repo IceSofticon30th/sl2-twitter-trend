51f2152c60997aa1f9aaf967601b76ab0da3604c
Yuto Ino <ice.softicon30th@gmail.com>
2015-10-14 11:12:36 +0900

    initial commit

---
+++ trend.js
@@ -0,0 +1,10 @@
+var Twitter = require('twitter');
+
+var client = new Twitter(require('./keys.json'));
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
