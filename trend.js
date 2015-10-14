c2adee43daa7d71bf7ae670e3badb893e603391b
Yuto Ino <ice.softicon30th@gmail.com>
2015-10-14 11:49:39 +0900

    agent

--- trend.js
+++ trend.js
@@ -1,10 +1,35 @@
 var Twitter = require('twitter');
+var express = require('express');
 
 var client = new Twitter(require('./keys.json'));
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/trend', function (req, res) {
+  var keyword = req.query.keyword;
+  
+  console.log(keyword);
+  
+  res.end(keyword);
+});
+
+app.listen(process.env.PORT || 3000);
+
+function searchTrend(callback) {
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      console.log(tweets);
+    }
+    if (typeof callback == 'function') callback();
+  });
+}
+
+var params = {q: ''};
+client.get('search/tweets', params, function(error, tweets, response){
   if (!error) {
     console.log(tweets);
   }
+  if (typeof callback == 'function') callback();
 });
